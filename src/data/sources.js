80695f0430b67e09b7f577b097c72b4e2379c368
vishalrk <vishalrk94@gmail.com>
2026-06-06 11:51:48 +0200

    full kiss md dataset and infrastructure tab

--- src/data/sources.js
+++ src/data/sources.js
@@ -38,5 +38,25 @@ export const dataSources = [
     dataset: "Daten_Gesundheit und Soziales",
     source: "Einwohnermelderegister",
     url: "https://statistik.magdeburg.de/KISS-MD/"
+  },
+  {
+    dataset: "Wohnungsbestand nach der Zahl der Räume und Jahren",
+    source: "KISS-MD",
+    url: "https://statistik.magdeburg.de/KISS-MD/"
+  },
+  {
+    dataset: "Fertiggestellte Wohnungen im Neubau in Wohn- u. Nichtwohngebäuden nach Stadtteilen",
+    source: "KISS-MD",
+    url: "https://statistik.magdeburg.de/KISS-MD/"
+  },
+  {
+    dataset: "Gebäuden mit Wohnraum (ohne Wohnheime)",
+    source: "KISS-MD",
+    url: "https://statistik.magdeburg.de/KISS-MD/"
+  },
+  {
+    dataset: "Leerstand im Geschosswohnungsbau",
+    source: "KISS-MD",
+    url: "https://statistik.magdeburg.de/KISS-MD/"
   }
 ];
